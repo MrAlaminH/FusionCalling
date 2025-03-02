19893aaabb6677d1a7a39e480b4af2977a62b4b1
Its-Alamin-H <alaminhosssain97@gmail.com>
2025-03-02 18:40:00 +0600

    feat: Adjust SlideText responsive text size for mobile and desktop

--- components/Slide-Text.tsx
+++ components/Slide-Text.tsx
@@ -6,7 +6,7 @@ export default function Slidetext() {
       <VelocityScroll
         text="Let's Build Together ❋"
         default_velocity={3}
-        className="mx-4 text-7xl font-extra-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent"
+        className="mx-4 text-5xl md:text-7xl font-extra-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent"
       />
     </div>
   );
